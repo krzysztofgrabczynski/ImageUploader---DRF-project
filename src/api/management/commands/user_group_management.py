3946e7ea8452a36d40f6872c0384334528be5201
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 17:39:35 +0200

    Create new command with functionality to create three permission gropus for users

---
+++ src/api/management/commands/user_group_management.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+
+
+class Command(BaseCommand):
+    """
+    A command class which creates a new permission groups. 
+    """
+
+    help = "Command for creating a new permission groups."
+
+    def handle(self, *args, **options):
+        group, created = Group.objects.get_or_create(name="Basic")
+        group, created = Group.objects.get_or_create(name="Premium")
+        group, created = Group.objects.get_or_create(name="Enterprise")
