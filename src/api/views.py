1027be2b3b9012b2d56529223608b8460dcdf299
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-21 00:07:26 +0200

    Functionality of the create method in 'ImageAPIView' (upload image and check user group)

--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, exceptions
 
 from api.models import ImageModel
 from api.serializers import ImageSerializer
+from api.tiers import BaseTierClass
 
 
 class ImageAPIView(generics.CreateAPIView):
@@ -11,6 +12,25 @@ class ImageAPIView(generics.CreateAPIView):
 
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        This method uploads a new image and check what tier/permission group user is belongs to for further specific operations.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        for tier in BaseTierClass.__subclasses__():
+            if request.user.groups.filter(name=tier.class_name()).exists():
+                response = tier.get_response(serializer)
+
+        try:
+            return response
+        except UnboundLocalError:
+            raise exceptions.PermissionDenied(
+                "You do not have permission or group permission"
+            )
 
 
 class ListImageAPIView(generics.ListAPIView):
