443b6e677e28a721d9dcee72b42009f6a2777136
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 22:56:39 +0200

    Code refactor using black tool

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -8,6 +8,7 @@ class ImageSerializer(serializers.ModelSerializer):
     A serializer class that serializes data for ImageModel model.
     A user field is hidden and default value is set to logged in user.
     """
+
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
