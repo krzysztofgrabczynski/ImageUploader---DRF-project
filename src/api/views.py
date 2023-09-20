dce75c832ff4304e9f3ddc7f786194b89f7e2e97
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 22:57:31 +0200

    New funcitonality for list all links of the uploaded images by the specific user

--- src/api/views.py
+++ src/api/views.py
@@ -5,5 +5,22 @@ from api.serializers import ImageSerializer
 
 
 class ImageAPIView(generics.CreateAPIView):
+    """
+    A APIView for POST method to upload a new image.
+    """
+
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializer
+
+
+class ListImageAPIView(generics.ListAPIView):
+    """
+    A APIView for GET method to list images of the specific logged in user.
+    """
+
+    queryset = ImageModel.objects.all()
+    serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
