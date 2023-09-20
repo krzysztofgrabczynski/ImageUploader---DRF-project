f463fd31313cc662759fba754f41d815cd7b112d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 22:57:43 +0200

    New url for list images

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views import ImageAPIView
+from api.views import ImageAPIView, ListImageAPIView
 
 
 urlpatterns = [
     path("upload/", ImageAPIView.as_view(), name="upload_image"),
+    path("list_images/", ListImageAPIView.as_view(), name="list_images"),
 ]
