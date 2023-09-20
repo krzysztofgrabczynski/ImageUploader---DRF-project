fb8a07e6d68e57c8a3ef660698f1303dc225eeeb
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 19:11:28 +0200

    Added docstring into ImageSerializer class

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -4,6 +4,10 @@ from api.models import ImageModel
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    """
+    A serializer class that serializes data for ImageModel model.
+    A user field is hidden and default value is set to logged in user.
+    """
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
