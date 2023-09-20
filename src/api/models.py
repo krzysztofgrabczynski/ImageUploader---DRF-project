2b4d04e0492f7580a829b010b1ca831137923d56
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-20 19:00:43 +0200

    Create ImageModel model with fields such as user and img (for uploading images)

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model() 
+
+
+class ImageModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to="images/")
