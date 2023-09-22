600921d50e1f5718e60bc3034922e09d8b166373
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-22 21:38:18 +0200

    New url for generated url for images

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from api.views import ImageAPIView, ListImageAPIView
+from api.views import ImageAPIView, ListImageAPIView, URLView
 
 
 urlpatterns = [
     path("upload/", ImageAPIView.as_view(), name="upload_image"),
     path("list_images/", ListImageAPIView.as_view(), name="list_images"),
+    path("<str:url_pk>/<int:image_pk>/", URLView.as_view(), name="url"),
 ]
