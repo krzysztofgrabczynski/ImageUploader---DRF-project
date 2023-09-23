d97808378fb3d96b800bc0ac658c478b12474e1f
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2023-09-23 14:52:53 +0200

    New url for craeting new account tiers

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from api.views import ImageAPIView, ListImageAPIView, URLView
+from api.views import ImageAPIView, ListImageAPIView, URLAPIVView, CreateAccountTierAPIView
 
 
 urlpatterns = [
     path("upload/", ImageAPIView.as_view(), name="upload_image"),
     path("list_images/", ListImageAPIView.as_view(), name="list_images"),
-    path("<str:url_pk>/<int:image_pk>/", URLView.as_view(), name="url"),
+    path("<str:url_pk>/<int:image_pk>/", URLAPIVView.as_view(), name="url"),
+    path("create_tier", CreateAccountTierAPIView.as_view(), name="create_tier"),
 ]
